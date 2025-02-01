42a9f853b419e5f6936acb78dd7a8839816157e9
Grace <wanggrace730@gmail.com>
2025-02-01 11:11:44 -0800

    next.js

---
+++ sustaingineering-web-app/pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+      <div>
+        <h1>Welcome to My Next.js App!</h1>
+        <p>This is my first page using Next.js</p>
+      </div>
+    );
+  }
